67cc06b8c71ded960b36c3f8d0c44a358b6756c0
RelaxedParrot311 <arthur.barboza0016@edu.unifil.br>
2021-06-16 17:22:37 -0300

    16/06/21 aula : 04/05/21 - Erro de Concatenação Resolvido

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  valor = 0 ; 
+  valor = "" ; 
 
   ckNumero (numero) {
-    this.valor = numero; 
+    this.valor += numero.toString (); 
 
 
   }
